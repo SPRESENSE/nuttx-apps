0009e4e5d00b668299c036ae4abdc2b81e7629c5
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2025-07-22 10:07:50 +0900

    netutils/esp8266: Fix nxstyle
    Fix nxstyle.

--- netutils/esp8266/esp8266.c
+++ netutils/esp8266/esp8266.c
@@ -2177,7 +2177,7 @@ int lesp_socket(int domain, int type, int protocol)
  *   close socket creates with lesp_socket.
  *
  * Input Parameters:
- *   sockfd   : socket indentifer.
+ *   sockfd    Socket descriptor returned by socket()
  *
  * Returned Value:
  *   A 0 on success; -1 on error.
